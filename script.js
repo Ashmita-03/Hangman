c0399e38599f338438e9faa7504e1fb3dee61899
Ashmita-03 <ashmitajyothilingam@gmail.com>
2025-10-25 21:24:48 +0000

    js

--- script.js
+++ script.js
@@ -6,3 +6,93 @@ const newGameButton = document.getElementById("new-game-button");
 const canvas = document.getElementById("canvas");
 const resultText = document.getElementById("result-text");
 
+let options = {
+  Food: [
+    "Sushi",
+    "Enchiladas",
+    "Pancakes",
+    "Biriyani",
+    "Lasagna",
+    "Hamburger"
+  ],
+  Technology: [
+    "Computer", 
+    "Internet", 
+    "Algorithm", 
+    "Database", 
+    "Software", 
+    "Programming"],
+  Festivals: [
+    "Diwali",
+    "Christmas",
+    "Thanksgiving",
+    "Ramadan",
+    "Easter",
+    "Hanukkah"
+  ],
+  Space: [
+    "Blackhole",
+    "Satellite",
+    "Asteroid",
+    "Astronaut",
+    "Constellation",
+    "Meteorite",
+    "Supernova",
+    "Universe"
+  ]
+
+};
+
+let winCount = 0;
+let count = 0;
+
+let chosenWord = "";
+
+
+const displayOptions = () => {
+  optionsContainer.innerHTML += `<h3>Please Select An Option</h3>`;
+  let buttonCon = document.createElement("div");
+  for (let value in options) {
+    buttonCon.innerHTML += `<button class="options" onclick="generateWord('${value}')">${value}</button>`;
+  }
+  optionsContainer.appendChild(buttonCon);
+};
+
+
+const blocker = () => {
+  let optionsButtons = document.querySelectorAll(".options");
+  let letterButtons = document.querySelectorAll(".letters");
+
+  optionsButtons.forEach((button) => {
+    button.disabled = true;
+  });
+
+  letterButtons.forEach((button) => {
+    button.disabled.true;
+  });
+  newGameContainer.classList.remove("hide");
+};
+
+const generateWord = (optionValue) => {
+  let optionsButtons = document.querySelectorAll(".options");
+
+  optionsButtons.forEach((button) => {
+    if (button.innerText.toLowerCase() === optionValue) {
+      button.classList.add("active");
+    }
+    button.disabled = true;
+  });
+
+
+  letterContainer.classList.remove("hide");
+  userInputSection.innerText = "";
+
+  let optionArray = options[optionValue];
+
+  chosenWord = optionArray[Math.floor(Math.random() * optionArray.length)];
+  chosenWord = chosenWord.toUpperCase();
+
+  let displayItem = chosenWord.replace(/./g, '<span class="dashes">_</span>');
+
+  userInputSection.innerHTML = displayItem;
+};
